7bb997db2e60e58469d015e8b980fe335cc15872
Uma <uma.kola143@gmail.com>
2024-12-18 16:42:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u umakola -p dckr_pat_P4VLdmm-o4kBmqtwAmkKvzVZYWc
-    docker tag test ar8888/dev
-    docker push ar8888/dev
+    docker login -u umakola -p dckr_pat_q_xg9rAiWwLynWr4b4pZMwZlmM4
+    docker tag test umakola/dev
+    docker push umakola/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u umakola -p dckr_pat_P4VLdmm-o4kBmqtwAmkKvzVZYWc
-    docker tag test ar8888/prod 
-    docker push ar8888/prod
+    docker login -u umakola -p dckr_pat_q_xg9rAiWwLynWr4b4pZMwZlmM4
+    docker tag test umakola/prod 
+    docker push umakola/prod
 fi
